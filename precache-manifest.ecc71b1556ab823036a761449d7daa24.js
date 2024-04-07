180514bb30d7e788ffc1b6133d0f9e505448942b
Lin Qiu <qiulin0128@gmail.com>
2024-04-07 01:50:07 -0700

    Updates

---
+++ precache-manifest.ecc71b1556ab823036a761449d7daa24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd685a0701d3ba3f6e71293162e07e1",
+    "url": "/fake-tweet-card/index.html"
+  },
+  {
+    "revision": "2594d89548b04fbac8c0",
+    "url": "/fake-tweet-card/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "d0d8b64ff2544f09e0d9",
+    "url": "/fake-tweet-card/static/css/main.5f92b110.chunk.css"
+  },
+  {
+    "revision": "2594d89548b04fbac8c0",
+    "url": "/fake-tweet-card/static/js/2.781fd8d4.chunk.js"
+  },
+  {
+    "revision": "2bf1c4b8fbf86feef53cba88c8403647",
+    "url": "/fake-tweet-card/static/js/2.781fd8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d8b64ff2544f09e0d9",
+    "url": "/fake-tweet-card/static/js/main.bc76aac0.chunk.js"
+  },
+  {
+    "revision": "ad4b5e1d731906d43672",
+    "url": "/fake-tweet-card/static/js/runtime-main.8e8d81b5.js"
+  }
+]);
